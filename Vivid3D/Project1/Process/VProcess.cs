bc85eb26bf522969a26d994f9ea5a2ddefb882ec
Anthony Robert Wells <scopedynamo@gmail.com>
2018-04-17 15:06:36 +0100

    initial commit

---
+++ Vivid3D/Project1/Process/VProcess.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace Vivid.Processes
+{
+    public enum ProcessType
+    {
+        Run,Wait
+    }
+    public class VProcess
+    {
+        public Process Pro = null;
+        public VProcess(string file,ProcessType type,string args="")
+        {
+            ProcessStartInfo si = new ProcessStartInfo(file)
+            {
+                WindowStyle = ProcessWindowStyle.Normal,
+                WorkingDirectory = "c:/vivid3d/",
+                CreateNoWindow = true,
+                Arguments = args
+            };
+
+            if (type == ProcessType.Wait)
+            {
+                using (Pro = Process.Start(si))
+                {
+                    Pro.WaitForExit();
+
+                }
+            }
+            else
+            {
+                Pro = Process.Start(si);
+            }
+
+        }
+    }
+}
